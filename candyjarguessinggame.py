a64b13c99c4b77e07fae4a1329b063f219f9fe21
lugadanielahjaneschleiden <luga.danielahjane.schleiden@gmail.com>
2026-01-21 06:39:12 +0800

    Add files via upload

---
+++ candyjarguessinggame.py
@@ -0,0 +1,29 @@
+import random
+
+def candy_jar_guessing_game_1to20():
+    candy_count = 12
+    guesses = 0
+    print("Tiny Candy Jar Guessing Game")
+    print("Only 1 to 20 candies inside. Can you guess how any?\n")
+while True:
+
+        user_guess = int(input("Your guess: ")) 
+        guesses =+1
+        if user_guess:int < max(candy_count = 11)
+        print("Way too few! guess way higher.\n")
+        if user_guess:int < 10
+        print("A little low! Add 1 or 2\n")
+        if user_guess:int > min (candy_count = 13)
+        print("Way too many! guess way lower.\n")
+        if user_guess:int > 14
+        print("A little high! Take 1 or 2.\n")
+        
+        print(f"\n YES! There are exactly (candy_count = 12) candies!") 
+        print(f"You got it in {guesses} tries - you win the tiny jar!")   
+        print("*hands you a cute little jar of mini candies* Yum!")    
+        "break"
+        Exception = ValueError 
+        print("Oops, that's not a number - try again!\n")     
+
+if "user_name" == "lastname_firstname ":
+      candy_jar_guessing_game_1to20
