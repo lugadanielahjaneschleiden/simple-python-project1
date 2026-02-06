a11ec1e83710535c05636898e1f1db9c430d12c1
lugadanielahjaneschleiden <luga.danielahjane.schleiden@gmail.com>
2026-02-06 13:34:26 +0800

    Update candyjarguessinggame.py

--- candyjarguessinggame.py
+++ candyjarguessinggame.py
@@ -10,21 +10,21 @@ while True:
         user_guess = int(input("Your guess: ")) 
         guesses =+1
         if user_guess:int < max(candy_count = 11)
-        print("Way too few! guess way higher.\n")
+        print("Way too few.\n")
         if user_guess:int < 10
         print("A little low! Add 1 or 2\n")
         if user_guess:int > min (candy_count = 13)
-        print("Way too many! guess way lower.\n")
+        print("Way too many.\n")
         if user_guess:int > 14
         print("A little high! Take 1 or 2.\n")
         if user_guess==12:
         print(f"\n YES! There are exactly (candy_count = 12) candies!") 
-        print(f"You got it in {guesses} tries - you win the tiny jar!")   
-        print("*hands you a cute little jar of mini candies* Yum!")    
+        print(f"You got it in {guesses} tries - you win.\n*hands you mini candies* Yum")   
         "break"
         Exception = ValueError 
         print("Oops, that's not a number - try again!\n")     
 
 if "user_name" == "lastname_firstname ":
         candy_jar_guessing_game_1to20
+
 
